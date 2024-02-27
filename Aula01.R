4b1e63b33564ba417ec4345bbee6426df3d1dd74
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2024-02-27 20:31:41 -0300

    Create Aula01.R

---
+++ Aula01.R
@@ -0,0 +1,30 @@
+#install.package("stringr")
+#library(stringr)
+
+h <- "Malcom"
+i <- "X"
+
+# vetor de string
+j <- c(h, i)
+j
+
+a <- 2
+d <- 4
+c <- a + d
+b <- "Salve"
+f <- "2"
+g <- 346
+
+print(c)
+
+print(3.1415)
+print(x = 3.1415, digits = 3)
+
+class(d)
+class(f)
+class(g)
+
+num <- c(0.1, 3242.20, 4.57, 56.40)
+num
+
+NomeCompleta <- str_c(h, i)
